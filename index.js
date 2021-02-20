d33d2e2ce5c4971df03c8707263e826e4775dced
Illya Havrulyk <peoso595@gmail.com>
2021-02-21 01:05:51 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import TelegramBot from "node-telegram-bot-api";
+import axios from "axios";
+import config from "config";
+
+const TOKEN = config.get("token");
+const bot = new TelegramBot(TOKEN, { polling: true })
+
+bot.on("message", message => {
+    const { chat: { id } } = message;
+    bot.sendMessage(id, "Init message");
+})
