d6d2dc9557aa225df31206a73336a46c0d6a0de1
Illya Havrulyk <peoso595@gmail.com>
2021-02-28 20:47:19 +0200

    Added message and keyboard on startup and made list command for keyboard.

--- index.js
+++ index.js
@@ -6,7 +6,37 @@ const TOKEN = config.get("token");
 const bot = new TelegramBot(TOKEN, { polling: true })
 const logo = "some logo";
 
-bot.on("message", message => {
+const product_list_keyboard = "List of products";
+
+const keyboard = [
+    [product_list_keyboard],
+    ["Home"]
+]
+
+bot.onText(/\/start/, message => {
     const { chat: { id } } = message;
-    bot.sendMessage(id, "Init message");
+    bot.sendMessage(id, "Greetings, you have now activated bot for storing items. Now you can use it's functionality via keyboard.", {
+        reply_markup: {
+            keyboard
+        }
+    })
+})
+
+bot.onText(new RegExp(`${product_list_keyboard}`), message => {
+    console.log(message);
+    const { chat: { id } } = message;
+    const message_text = message.text;
+    switch (message_text) {
+        case product_list_keyboard:
+            axios.get("http://localhost:8080/product/list").then(res => {
+
+                let { data } = res;
+                console.log(data);
+                data.map((item, index) => {
+                    bot.sendPhoto(id, item.imgUrl, {
+                        caption: `${item.name}\n\n${item.description}\nDate added : ${item.dateAdded}\nQuantity : ${item.quantity}\nPrice : ${item.price}`
+                    })
+                })
+            })
+    }
 })
