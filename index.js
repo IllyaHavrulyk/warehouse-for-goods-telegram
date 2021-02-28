a98a62685d3c0eea4d256c6dda518db9cae530c4
Illya Havrulyk <peoso595@gmail.com>
2021-03-01 00:49:34 +0200

    Refactored keyboard, added inline keyboard for every photo in list, added emojis to every keyboard string.

--- index.js
+++ index.js
@@ -6,12 +6,22 @@ const TOKEN = config.get("token");
 const bot = new TelegramBot(TOKEN, { polling: true })
 const logo = "some logo";
 
-const product_list_keyboard = "List of products";
+const PRODUCT_LIST_KEYBOARD = `List of products ${String.fromCodePoint(0x1f4f0)}`;
+const PRODUCT_DELETE_KEYBOARD = `Delete certain product. ${String.fromCodePoint(0x274c)}`;
+const PRODUCT_UPDATE_KEYBOARD = `Edit certain product. 	${String.fromCodePoint(0x270f)}`;
+const PRODUCT_CREATE_KEYBOARD = `Create product. ${String.fromCodePoint(0x2705)}`;
+const PRODUCT_GET_KEYBOARD = `Get some product. ${String.fromCodePoint(0x1f504)}`;
 
 const keyboard = [
-    [product_list_keyboard],
-    ["Home"]
+    [PRODUCT_GET_KEYBOARD],
+    [PRODUCT_UPDATE_KEYBOARD, PRODUCT_CREATE_KEYBOARD, PRODUCT_DELETE_KEYBOARD],
+    [PRODUCT_LIST_KEYBOARD],
+    [`Home ${String.fromCodePoint(0x21aa)}`]
 ]
+
+const PRODUCT_GET_CALLBACK = "Get product by ID";
+const PRODUCT_DELETE_CALLBACK = "Delete product by ID";
+const PRODUCT_UPDATE_CALLBACK = "Update product by ID with input data.";
 
 bot.onText(/\/start/, message => {
     const { chat: { id } } = message;
@@ -22,19 +32,42 @@ bot.onText(/\/start/, message => {
     })
 })
 
-bot.onText(new RegExp(`${product_list_keyboard}`), message => {
+bot.onText(new RegExp(`${PRODUCT_LIST_KEYBOARD}(.*)`), message => {
     console.log(message);
     const { chat: { id } } = message;
     const message_text = message.text;
     switch (message_text) {
-        case product_list_keyboard:
+        case PRODUCT_LIST_KEYBOARD:
             axios.get("http://localhost:8080/product/list").then(res => {
 
                 let { data } = res;
                 console.log(data);
                 data.map((item, index) => {
                     bot.sendPhoto(id, item.imgUrl, {
-                        caption: `${item.name}\n\n${item.description}\nDate added : ${item.dateAdded}\nQuantity : ${item.quantity}\nPrice : ${item.price}`
+                        parseMode: "Markup",
+                        caption: `${item.name}\n\n${item.description}\nDate added : ${item.dateAdded}\nQuantity : ${item.quantity}\nPrice : ${item.price}`,
+                        reply_markup: {
+                            inline_keyboard: [
+                                [
+                                    {
+                                        text: `Get this product ${String.fromCodePoint(0x1f504)}`,
+                                        callback_data: `${PRODUCT_GET_CALLBACK} ${item.id}`
+                                    }
+                                ],
+                                [
+                                    {
+                                        text: `Edit this product ${String.fromCodePoint(0x270f)}`,
+                                        callback_data: `${PRODUCT_UPDATE_CALLBACK} ${item.id}}`
+                                    }
+                                ],
+                                [
+                                    {
+                                        text: `Delete this product ${String.fromCodePoint(0x274c)}`,
+                                        callback_data: `${PRODUCT_DELETE_CALLBACK} ${item.id}`
+                                    }
+                                ]
+                            ]
+                        }
                     })
                 })
             })
